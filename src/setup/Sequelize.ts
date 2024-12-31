8a9e9a93db3e1a2f396a85315a5d68c93eac7bb6
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-12-31 12:59:52 +0100

    adjust db connection

--- src/setup/Sequelize.ts
+++ src/setup/Sequelize.ts
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
-
 import dotenv from "dotenv";
+import fs from "fs";
 dotenv.config();
 
 const DB_NAME = process.env.DB_NAME;
@@ -8,11 +8,17 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_CONNECTION = process.env.DB_CONNECTION;
-
+const SSL_CERT_PATH = process.env.SSL_CERT_PATH || './ca.pem';
 
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
-  dialect: DB_CONNECTION
+  dialect: DB_CONNECTION,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      ca: fs.readFileSync(SSL_CERT_PATH).toString(),
+    },
+  },
 });
 
 sequelize
@@ -20,7 +26,7 @@ sequelize
   .then(() => {
     console.log('Connection has been established successfully.');
   })
-  .catch((err:any) => {
+  .catch((err: any) => {
     console.error('Unable to connect to the database:', err);
   });
 
