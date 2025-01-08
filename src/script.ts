3d2611822b51b2876f540f438050ffa1fa892fe2
Ashiru Olanrewaju <lanre2967@gmail.com>
2025-01-08 17:11:17 +0100

    cors fix

--- src/script.ts
+++ src/script.ts
@@ -88,8 +88,12 @@ interface corsInterface {
   allowedHeaders?: string[];
 }
 
+const allowedCorsUrls = FRONTEND_URL
+    ? FRONTEND_URL.split(',')
+    : [];
+
 const corsOption: corsInterface = {
-  origin: FRONTEND_URL,
+  origin: allowedCorsUrls,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
